2cfc25d32df2b558d07f8ca7afd7a38b036c3ead
Flombo <florian-pfuetzenreuter@hotmail.de>
2018-12-29 14:12:40 +0100

    Puzzle game prototype

--- src/GameInit.java
+++ src/GameInit.java
@@ -6,8 +6,6 @@ public class GameInit {
 
     private int height;
     private int width;
-    public JPanel panel;
-    private JFrame frame;
     private Field[][] fields;
     private int xDimension;
     private int yDimension;
@@ -26,35 +24,34 @@ public class GameInit {
         this.playerPosY = this.randomPos();
     }
 
-    public void setPlayerPosX(int x){
+    public void setPlayerPosX(int x) {
         this.playerPosX = x;
+
     }
 
-    public void setPlayerPosY(int y){
-        this.playerPosX = y;
+    public void setPlayerPosY(int y)
+    {
+        this.playerPosY = y;
     }
 
-    public int getHeight() {
+    public int getHeight()
+    {
         return height;
     }
 
-    public void setHeight(int height) {
+    public void setHeight(int height)
+    {
         this.height = height;
     }
 
     public int getWidth() {
+
         return width;
     }
 
     public void setWidth(int width) {
-        this.width = width;
-    }
 
-    //Initialize Frame
-    void initFrame(){
-        this.frame = new JFrame();
-        this.frame.getContentPane();
-        this.frame.setVisible(true);
+        this.width = width;
     }
 
     //Create Fields
@@ -108,58 +105,58 @@ public class GameInit {
         this.fields[this.playerPosX][this.playerPosY] = player;
     }
 
-    //keylistener for player-movement
-    void changePlayerPos(KeyEvent event){
-        int keyCode = event.getKeyCode();
-        switch ( keyCode ){
-            case KeyEvent.VK_DOWN:
-                this.movePlayer(true, -1);
-                System.out.println("down");
-                this.printAllFields();
-                break;
-            case KeyEvent.VK_UP:
-                this.movePlayer(true, 1);
-                System.out.println("up");
-                this.printAllFields();
-                break;
-            case KeyEvent.VK_RIGHT:
-                this.movePlayer(false, 1);
-                System.out.println("right");
-                this.printAllFields();
-                break;
-            case KeyEvent.VK_LEFT:
-                this.movePlayer(false, -1);
-                System.out.println("left");
-                this.printAllFields();
-                break;
-            default:
-                System.out.println("You have to press the arrow keys!");
-                this.printAllFields();
-                break;
-        }
-
-    }
-
-    //method that checks if down/up is pressed and if player reached the end of field
-    private void movePlayer(boolean upDown,int newPos) {
-        if(upDown) {
-            if (!(this.fields[this.playerPosX][this.playerPosY + newPos].getY() < 0) || !(this.fields[this.playerPosX][this.playerPosY + newPos].getY() > 0)) {
-                this.fields[this.playerPosX][this.playerPosY] = new Field(this.playerPosX, this.playerPosY, "Field");
-                this.setPlayerPosY(this.playerPosY + newPos);
-                this.movePlayerPos();
-            } else {
-                System.out.println("You reached the end.");
-            }
-        }else {
-            if (!(this.fields[this.playerPosX + newPos][this.playerPosY].getX() < 0) || !(this.fields[this.playerPosX + newPos][this.playerPosY].getX() > 0)) {
-                this.fields[this.playerPosX][this.playerPosY] = new Field(this.playerPosX, this.playerPosY, "Field");
-                this.setPlayerPosX(this.playerPosX + newPos);
-                this.movePlayerPos();
-            } else {
-                System.out.println("You reached the end.");
-            }
-        }
-    }
+//    //keylistener for player-movement
+//    void changePlayerPos(KeyEvent event){
+//        int keyCode = event.getKeyCode();
+//        switch ( keyCode ){
+//            case KeyEvent.VK_DOWN:
+//                this.movePlayer(true, -1);
+//                System.out.println("down");
+//                this.printAllFields();
+//                break;
+//            case KeyEvent.VK_UP:
+//                this.movePlayer(true, 1);
+//                System.out.println("up");
+//                this.printAllFields();
+//                break;
+//            case KeyEvent.VK_RIGHT:
+//                this.movePlayer(false, 1);
+//                System.out.println("right");
+//                this.printAllFields();
+//                break;
+//            case KeyEvent.VK_LEFT:
+//                this.movePlayer(false, -1);
+//                System.out.println("left");
+//                this.printAllFields();
+//                break;
+//            default:
+//                System.out.println("You have to press the arrow keys!");
+//                this.printAllFields();
+//                break;
+//        }
+//
+//    }
+//
+//    //method that checks if down/up is pressed and if player reached the end of field
+//    private void movePlayer(boolean upDown,int newPos) {
+//        if(upDown) {
+//            if (!(this.fields[this.playerPosX][this.playerPosY + newPos].getY() < 0) || !(this.fields[this.playerPosX][this.playerPosY + newPos].getY() > 0)) {
+//                this.fields[this.playerPosX][this.playerPosY] = new Field(this.playerPosX, this.playerPosY, "Field");
+//                this.setPlayerPosY(this.playerPosY + newPos);
+//                this.movePlayerPos();
+//            } else {
+//                System.out.println("You reached the end.");
+//            }
+//        }else {
+//            if (!(this.fields[this.playerPosX + newPos][this.playerPosY].getX() < 0) || !(this.fields[this.playerPosX + newPos][this.playerPosY].getX() > 0)) {
+//                this.fields[this.playerPosX][this.playerPosY] = new Field(this.playerPosX, this.playerPosY, "Field");
+//                this.setPlayerPosX(this.playerPosX + newPos);
+//                this.movePlayerPos();
+//            } else {
+//                System.out.println("You reached the end.");
+//            }
+//        }
+//    }
 
     //Prints all Fields after everything is set
     void printAllFields(){
